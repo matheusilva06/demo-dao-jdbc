cce2c847546771d4cd0dc514db71a68fc0b2dc49
matheusilva06 <matheus.s.souza06@gmail.com>
2025-10-08 22:19:58 -0300

    Reusing instantiaton

--- src/application/Program.java
+++ src/application/Program.java
@@ -15,6 +15,7 @@ public class Program {
 		
 		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
 		
+		System.out.println("==== TEST 1: seller findById ==== ");
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		System.out.println(seller);
