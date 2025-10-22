ef49aeff80ed891e49a42697b3aae13230bc81cb
matheusilva06 <matheus.s.souza06@gmail.com>
2025-10-21 22:38:02 -0300

    Implemented insert, update, deleteById, findById and findAll methods in DepartmentDaoJDBC

--- src/model/dao/impl/DepartmentDaoJDBC.java
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -5,6 +5,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 import db.DB;
@@ -15,7 +16,7 @@ import model.entities.Department;
 public class DepartmentDaoJDBC implements DepartmentDao {
 
 	private Connection conn;
-	
+
 	public DepartmentDaoJDBC(Connection conn) {
 		super();
 		this.conn = conn;
@@ -24,58 +25,139 @@ public class DepartmentDaoJDBC implements DepartmentDao {
 	@Override
 	public void insert(Department obj) {
 		PreparedStatement st = null;
+		ResultSet rs = null;
 		try {
-			 st = conn.prepareStatement(
-					 "INSERT INTO department "
-					 + "(Name) "
-					 + "VALUES (?)",
-					 Statement.RETURN_GENERATED_KEYS);
-			 
-			 st.setString(1, obj.getName());
-			 
-			 int rowsAffected = st.executeUpdate();
-			 
-			 if(rowsAffected > 0) {
-				 ResultSet rs = st.getGeneratedKeys();
-				 if (rs.next()) {
-					 int id = rs.getInt(1);
-					 obj.setId(id);
-				 } 
-				 DB.closeResultSet(rs);
-			 } else {
-				 throw new DbException("Unexpected error! No rows affected!");
-			 }
-			 
-		} catch(SQLException e) {
+			st = conn.prepareStatement("INSERT INTO department " + "(Name) " + "VALUES (?)",
+					Statement.RETURN_GENERATED_KEYS);
+
+			st.setString(1, obj.getName());
+
+			int rowsAffected = st.executeUpdate();
+
+			if (rowsAffected > 0) {
+				 rs = st.getGeneratedKeys();
+				if (rs.next()) {
+					int id = rs.getInt(1);
+					obj.setId(id);
+				}
+			} else {
+				throw new DbException("Unexpected error! No rows affected!");
+			}
+
+		} catch (SQLException e) {
 			throw new DbException(e.getMessage());
-			
+
 		} finally {
+			DB.closeResultSet(rs);
 			DB.closeStatement(st);
 		}
-		
+
 	}
 
 	@Override
 	public void update(Department obj) {
-		// TODO Auto-g generated method stub
-		
+		PreparedStatement st = null;
+		try {
+			st = conn.prepareStatement("UPDATE department " + "SET Name = ? " + "WHERE Id = ?"
+					);
+			st.setString(1, obj.getName());
+			st.setInt(2, obj.getId());
+
+			int rowsAffected = st.executeUpdate();
+
+			if (rowsAffected == 0) {
+				throw new DbException("No department found with the given ID!");
+			}
+
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
+			DB.closeStatement(st);
+		}
+
 	}
+
 	@Override
 	public void deleteById(Integer id) {
-		// TODO Auto-generated method stub
+		PreparedStatement st = null;
+		try {
+		st = conn.prepareStatement(
+				"DELETE FROM department "
+				+ "WHERE Id = ?"
+				);
+		
+		st.setInt(1, id);
+		
+		int rowsAffected = st.executeUpdate();
+		
+		if (rowsAffected == 0) {
+			throw new DbException("No department found with the given ID!");
+		}
+	} catch (SQLException e) {
+		throw new DbException(e.getMessage());
+	} finally {
+		DB.closeStatement(st);
+	}
 		
 	}
 
 	@Override
 	public Department findById(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+			st = conn.prepareStatement(
+					"SELECT * FROM department "
+					+ "WHERE Id = ?"
+					);
+			
+			st.setInt(1, id);
+			
+			rs = st.executeQuery();
+
+		    if (rs.next()) {
+	            Department dep = new Department();
+	            dep.setId(rs.getInt("Id"));
+	            dep.setName(rs.getString("Name"));
+	            return dep;
+	        }
+		    return null; 
+	        
+		}catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
+			DB.closeResultSet(rs);
+			DB.closeStatement(st);
+		}
 	}
 
 	@Override
 	public List<Department> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+		st = conn.prepareStatement(
+				"SELECT * FROM department "
+				+ "ORDER BY Name"
+				);
+		
+		rs = st.executeQuery();
+
+        List<Department> list = new ArrayList<>();
+        
+        while (rs.next()) {
+            Department dep = new Department();
+            dep.setId(rs.getInt("Id"));
+            dep.setName(rs.getString("Name"));
+            list.add(dep);
+        }
+        
+        return list;
+	} catch(SQLException e) {
+		throw new DbException(e.getMessage());
+	} finally {
+		DB.closeStatement(st);
 	}
+}
 
 }
